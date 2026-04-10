85db67024bf991dd6e45df91aefae1c728f17caf
Jaro29 <jarofieducik@gmail.com>
2026-04-10 22:01:44 +0200

    build: instalacja Tailwind CSS v4 i utworzenie struktury Angulara

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,9 +3,9 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'frontend';
